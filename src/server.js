d3f770a53b9d6bffc76a388ffa91a7c0d0d22162
Pannnomaly <supawith007@gmail.com>
2025-12-23 14:23:56 +0700

    hotfix: port 3000

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 
 const app = express();
+const PORT = "3000";
 
 // ได้แล้ว 1 endpoint
 app.get('/', (req, res) => {
     res.send("Hello World!, I am your Server!");
 });
 
-app.listen("3000", () => {
+app.listen(PORT, () => {
     console.log("Server running on port 3000!");
 });
